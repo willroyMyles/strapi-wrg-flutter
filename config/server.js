191bd4825c9dd26364ad8f30a7c1079380b20a8b
Willroy Myles <willroymyles@yahoo.com>
2021-05-09 22:01:38 -0500

    Added post, userinfo models and added mongodb support

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9600921748924a5188e3b8d234e58182'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5ac324d5-f955-4b0e-b350-fd0025505984'
+};
